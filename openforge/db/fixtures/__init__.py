d4730d9a733dcf40b08ee93af0d236530db79287
Devon Jones <devon.jones@gmail.com>
2025-01-22 22:49:31 -0700

    switching fixtures to real data

--- openforge/db/fixtures/__init__.py
+++ openforge/db/fixtures/__init__.py
@@ -14,6 +14,7 @@ except ImportError:
 
 import openforge.db.sql.blueprints as blueprint_sql
 import openforge.db.sql.tags as tag_sql
+import openforge.db.sql.images as image_sql
 
 
 def find_fixtures():
@@ -76,9 +77,16 @@ def load_fixture(curs: cursor, data: dict):
         bp = blueprint_sql.insert_blueprint(curs, bp_data, rescue_md5_conflict=True)
         for tag in data.get("tags", []):
             tag_sql.insert_tag(curs, bp["id"], _munge_tag(tag))
+        for image in data.get("images", []):
+            image_sql.insert_image_for_blueprint(curs, bp["id"], _munge_image(image))
     except UniqueViolation:
         sys.stderr.write(f"MD5 not unique for {bp_data['blueprint_name']}\n")
 
 
 def _munge_tag(tag: list):
     return "|".join(str(t) for t in tag)
+
+
+def _munge_image(image: dict):
+    # placeholder for additional work if needed
+    return image
---
+++ openforge/db/sql/images.py
@@ -0,0 +1,67 @@
+import uuid
+from pprint import pprint
+from psycopg import cursor, sql
+
+
+def get_image_by_id(curs: cursor, image_id: uuid.UUID) -> dict:
+    query = sql.SQL(
+        """
+SELECT id, image_name, image_url, created_at, updated_at
+  FROM images
+  WHERE id = {image_id}
+"""
+    ).format(image_id=sql.Literal(image_id))
+    curs.execute(query)
+    return curs.fetchone()
+
+
+def insert_image(curs: cursor, image_name: str, image_url: str) -> dict:
+    query = sql.SQL(
+        """
+INSERT INTO images (
+  image_name, image_url
+) VALUES (
+  {image_name}, {image_url}
+) ON CONFLICT DO NOTHING
+RETURNING id
+"""
+    ).format(image_name=sql.Literal(image_name), image_url=sql.Literal(image_url))
+    curs.execute(query)
+    return get_image_by_id(curs, curs.fetchone()["id"])
+
+
+def get_blueprint_image_by_id(curs: cursor, blueprint_image_id: uuid.UUID) -> dict:
+    query = sql.SQL(
+        """
+SELECT id, blueprint_id, image_id, created_at, updated_at
+  FROM blueprint_images
+  WHERE id = {blueprint_image_id}
+"""
+    ).format(blueprint_image_id=sql.Literal(blueprint_image_id))
+    curs.execute(query)
+    return curs.fetchone()
+
+
+def insert_blueprint_image(
+    curs: cursor, blueprint_id: uuid.UUID, image_id: uuid.UUID
+) -> dict:
+    query = sql.SQL(
+        """
+INSERT INTO blueprint_images (
+  blueprint_id, image_id
+) VALUES (
+  {blueprint_id}, {image_id}
+) ON CONFLICT DO NOTHING
+RETURNING id
+"""
+    ).format(blueprint_id=sql.Literal(blueprint_id), image_id=sql.Literal(image_id))
+    curs.execute(query)
+    return get_blueprint_image_by_id(curs, curs.fetchone()["id"])
+
+
+def insert_image_for_blueprint(
+    curs: cursor, blueprint_id: uuid.UUID, image: dict
+) -> dict:
+    image = insert_image(curs, image["image_name"], image["image_url"])
+    insert_blueprint_image(curs, blueprint_id, image["id"])
+    return image
