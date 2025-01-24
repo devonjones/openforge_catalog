b7f64f798860e780ab5cd163753fd747970e81d1
Devon Jones <devon.jones@gmail.com>
2025-01-24 11:20:52 -0700

    added auth token to write endpoints until we have something better for AAA

--- openforge/app/__init__.py
+++ openforge/app/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask, g
+import os
+
+from flask import Flask
+
 from openforge.db import PgDB
 
 
 def init_app(app: Flask):
     db = PgDB(app.config)
+    app.config["API_TOKEN"] = os.environ.get("API_TOKEN", "1234567890")
     app.db = db
