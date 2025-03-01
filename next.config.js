4606fad70d9af616cada597a9d3d2a08c6608376
Devon Jones <devon.jones@gmail.com>
2025-03-01 12:39:48 -0700

    fixed the build

--- next.config.js
+++ next.config.js
@@ -5,12 +5,15 @@ const nextConfig = {
 
 if (process.env.BACKEND_API) {
   nextConfig.rewrites = async () => {
-      return [
-          {
-              source: '/api/:path*',
-              destination: `${process.env.BACKEND_API}/:path*`
-          }
-      ]
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
+      },
+    ]
   }
 }
 
