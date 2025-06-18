06b5689e3ff22383226e0bccc9d8baa9898660d3
Devon Jones <devon.jones@gmail.com>
2025-06-18 11:06:48 -0600

    first chunk of react tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['**/__tests__/**/*.(test|spec).(ts|tsx|js|jsx)'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+}; 
